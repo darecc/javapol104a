1bd18d21e3855cd3d58feed994865421d66175d3
darecc <dariusz.ceglarek@gmail.com>
2021-11-28 19:19:03 +0100

    4

--- src/java104/Main.java
+++ src/java104/Main.java
@@ -2,6 +2,6 @@ package java104;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ala ma kota");
+        System.out.println("Ala ma kota a Ola ma psa");
     }
 }
